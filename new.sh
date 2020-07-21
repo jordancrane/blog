ecbec5517ba46c61b7ae41000cfcb98002736252
Jordan Crane <jordan@jordancrane.me>
2020-07-21 12:31:17 -0700

    Restructure URIs, modify new post script, match bg color to homepage

---
+++ new.sh
@@ -0,0 +1,31 @@
+function newmicropost() {
+    local year=`date +%Y`
+    local month=`date +%m`
+    local day=`date +%d`
+    local time=`date +%H%M%S%z`
+    local file="microposts/${year}/${month}/${day}/${time}.md"
+
+    vim `hugo new ${file} | cut -d ' ' -f 1`
+}
+
+# $1 The name of the file to create
+function newpost() {
+    local year=`date +%Y`
+    local month=`date +%m`
+    local day=`date +%d`
+    local time=`date +%H%M%S%z`
+    local file="posts/${year}/${month}/${day}/${time}.md"
+
+    vim `hugo new ${file} | cut -d ' ' -f 1`
+}
+
+# $1 The name of the file to create
+function newphotopost() {
+  local year=`date +%Y`
+  local month=`date +%m`
+  local day=`date +%d`
+  local time=`date +%H%M%S%z`
+  local file="photos/${year}/${month}/${day}/${time}.md"
+
+	vim `hugo new ${file} | cut -d ' ' -f 1`
+}
